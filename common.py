ba212f9663b7d7451c6046ebaea1e1994d2280fd
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-10-26 18:31:32 +0100

    use a programming language

--- common.py
+++ common.py
@@ -2,3 +2,7 @@ import subprocess
 
 def run(*args, **kwargs):
     return subprocess.run(*args, check = True, **kwargs)
+
+def thisbranch():
+    line, = run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout = subprocess.PIPE).stdout.decode().splitlines()
+    return line
