8e45f9e93a87168a7800141c8be2a2192fcfd617
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-10-30 17:06:41 +0000

    make importable

--- common.py
+++ common.py
@@ -2,6 +2,16 @@ import subprocess, os, sys
 
 infodirname = '.pb'
 
+def pb():
+    path = os.path.join(findproject(), infodirname, thisbranch())
+    if os.path.exists(path):
+        with open(path) as f:
+            line = f.readline()
+    else:
+        line = run(['git', 'rev-list', '--max-parents=0', 'HEAD'], stdout = subprocess.PIPE).stdout.decode()
+    line, = line.splitlines()
+    return line
+
 def run(*args, **kwargs):
     return subprocess.run(*args, check = True, **kwargs)
 
--- pb
+++ pb
@@ -4,15 +4,5 @@
 
 from common import *
 
-def pb():
-    path = os.path.join(findproject(), infodirname, thisbranch())
-    if os.path.exists(path):
-        with open(path) as f:
-            line = f.readline()
-    else:
-        line = run(['git', 'rev-list', '--max-parents=0', 'HEAD'], stdout = subprocess.PIPE).stdout.decode()
-    line, = line.splitlines()
-    return line
-
 if '__main__' == __name__:
     print(pb())
