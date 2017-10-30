2d36c1cee974797c83448b9638465216fc58f7cf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-10-30 15:38:00 +0000

    find gradlew for me

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, os
 
 def run(*args, **kwargs):
     return subprocess.run(*args, check = True, **kwargs)
@@ -6,3 +6,12 @@ def run(*args, **kwargs):
 def thisbranch():
     line, = run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout = subprocess.PIPE).stdout.decode().splitlines()
     return line
+
+def findproject():
+    name = path = '.git'
+    while not os.path.exists(path):
+        parent = '..' + os.sep + path
+        if os.path.abspath(parent) == os.path.abspath(path):
+            raise Exception('No project found.')
+        path = parent
+    return '.' if path == name else path[:-len(os.sep + name)]
