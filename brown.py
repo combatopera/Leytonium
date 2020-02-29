12c9e078110e3380b9b67bcac7af3d43cb8858b7
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-02-29 19:50:29 +0000

    port some to lagoon

--- brown.py
+++ brown.py
@@ -1,17 +1,17 @@
 from dev_bin.common import findproject
+from lagoon import autopep8
 import subprocess, re
 
 cols = 120
 
 def main_brown():
     'Satisfy PEP 8 with minimal impact.'
-    command = ['autopep8', '-rv', '--max-line-length', str(cols), findproject()]
-    result = subprocess.run(command + ['-d'], stdout = subprocess.DEVNULL, stderr = subprocess.PIPE, universal_newlines = True)
-    assert not result.returncode
+    command = '-rv', '--max-line-length', cols, findproject()
+    result = autopep8(*command, '-d', stdout = subprocess.DEVNULL, stderr = subprocess.PIPE)
     def paths():
-        for line in result.stderr.splitlines():
+        for line in result.splitlines():
             m = re.fullmatch(r'\[file:(.+)]', line)
             if m is not None:
                 yield m.group(1)
     subprocess.check_call(['sed', '-ni', r'/\S/p'] + list(paths()))
-    subprocess.check_call(command + ['-i'])
+    autopep8.print(*command, '-i')
