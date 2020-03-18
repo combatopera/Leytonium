7d4584ec8bc3785a02e3d93046c184ec9ea58567
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 08:46:57 +0000

    lagoon

--- abandon.py
+++ abandon.py
@@ -1,7 +1,7 @@
-from dev_bin.common import run
+from lagoon import git
 
 def main_abandon():
     'Discard all local changes, with confirmation step.'
-    run(['git', 'status'])
+    git.status.print()
     input('Press enter to permanently lose all these changes.')
-    run(['git', 'reset', '--hard'])
+    git.reset.__hard.print()
