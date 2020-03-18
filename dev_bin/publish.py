f2ac58cbcc81fd4e88adaa53f599afc8b523b5b2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 21:42:46 +0000

    runlines

--- dev_bin/publish.py
+++ dev_bin/publish.py
@@ -1,7 +1,7 @@
-from .common import thisbranch, pb, args, runlines
+from .common import thisbranch, pb, args
 from lagoon import git
 
 def main_publish():
     'Publish this branch, accepts push options.'
-    remote, = runlines(['git', 'config', '--get', "branch.%s.remote" % pb()])
+    remote, = git.config.__get("branch.%s.remote" % pb()).splitlines()
     git.push._u.print(remote, thisbranch(), *args())
