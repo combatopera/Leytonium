bd3e48d6430f2f8eab635b3fce7dda0daa4c34a8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 20:39:41 +0000

    lagoon

--- dev_bin/dp.py
+++ dev_bin/dp.py
@@ -1,7 +1,8 @@
-from .common import getpublic, args, chain, stderr
+from .common import getpublic, args, stderr
+from lagoon import git
 
 def main_dp():
     'Diff from public branch.'
     parent = getpublic()
     stderr("Public branch: %s" % parent)
-    chain(['git', 'diff', '-M25'] + args() + [parent])
+    git.diff._M25.exec(*args(), parent)
