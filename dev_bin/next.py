57432a344eb53bf792eae0e55a0788d118cce046
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 19:55:30 +0000

    lagoon

--- dev_bin/next.py
+++ dev_bin/next.py
@@ -1,4 +1,5 @@
-from .common import findproject, run, runlines
+from .common import findproject, runlines
+from lagoon import git
 import os
 
 def main_next():
@@ -6,14 +7,14 @@ def main_next():
     gitdir = os.path.join(findproject(), '.git')
     if os.path.isdir(os.path.join(gitdir, 'rebase-apply')):
         if runlines(['git', 'status', '--porcelain']):
-            run(['git', 'rebase', '--continue'])
+            git.rebase.__continue.print()
         else:
-            run(['git', 'rebase', '--skip'])
+            git.rebase.__skip.print()
     elif os.path.isfile(os.path.join(gitdir, 'MERGE_HEAD')):
-        run(['git', 'commit', '--no-edit'])
+        git.commit.__no_edit.print()
     elif os.path.isfile(os.path.join(gitdir, 'CHERRY_PICK_HEAD')):
-        run(['git', 'cherry-pick', '--continue'])
+        git.cherry_pick.__continue.print()
     elif os.path.isfile(os.path.join(gitdir, 'REVERT_HEAD')):
-        run(['git', 'revert', '--continue'])
+        git.revert.__continue.print()
     else:
         raise Exception('Unknown git workflow, giving up.')
