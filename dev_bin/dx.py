0cb9d2b1e2cf4818113f8ffe232a9f136af959bd
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 20:15:05 +0000

    merge

--- dev_bin/dx.py
+++ dev_bin/dx.py
@@ -1,7 +1,21 @@
-from .common import pb, args as getargs, chain, stderr
+from .common import pb, args as getargs, chain, stderr, AllBranches, showmenu
 
 def main_dx():
     'Diff from parent branch.'
     parent = pb()
     stderr("Parent branch: %s" % parent)
     chain(['git', 'diff', '-M25'] + getargs() + [parent])
+
+def main_dxx():
+    'Short diff from parent branch or of passed-in commit number.'
+    args = getargs()
+    if args:
+        n, = args
+        n = int(n)
+        commit = showmenu(AllBranches().branchcommits(), False)[n]
+        commits = ["%s^" % commit, commit]
+    else:
+        parent = pb()
+        stderr("Parent branch: %s" % parent)
+        commits = [parent]
+    chain(['git', 'diff', '-M25', '--name-status'] + commits)
