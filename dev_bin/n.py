2ee2960c03299afe875bf99e4fa708d94284aa97
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 21:41:11 +0000

    runlines

--- dev_bin/n.py
+++ dev_bin/n.py
@@ -1,9 +1,8 @@
-from .common import runlines
-from lagoon import co, st
+from lagoon import co, git, st
 
 def main_n():
     'Switch to the next branch and run st.'
-    lines = runlines(['git', 'branch'])
+    lines = git.branch().splitlines()
     for i, line in enumerate(lines):
         if line.startswith('*'):
             b = lines[i + 1].strip()
