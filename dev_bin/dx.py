77df27e78599167abfc336ec7fcaead0739b0aec
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 20:14:20 +0000

    refactoring

--- dev_bin/dx.py
+++ dev_bin/dx.py
@@ -1,7 +1,7 @@
-from .common import pb, args, chain, stderr
+from .common import pb, args as getargs, chain, stderr
 
 def main_dx():
     'Diff from parent branch.'
     parent = pb()
     stderr("Parent branch: %s" % parent)
-    chain(['git', 'diff', '-M25'] + args() + [parent])
+    chain(['git', 'diff', '-M25'] + getargs() + [parent])
