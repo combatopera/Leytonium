078d045eb0726eaa40ef9d8eb7ffbf53c6686428
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-18 21:23:05 +0000

    lagoon

--- dev_bin/ren.py
+++ dev_bin/ren.py
@@ -1,10 +1,11 @@
-from .common import thisbranch, args, findproject, infodirname, run
+from .common import thisbranch, args, findproject, infodirname
+from lagoon import git
 import os
 
 def main_ren():
     'Rename current branch.'
     fromname = thisbranch()
-    run(['git', 'branch', '-m'] + args())
+    git.branch._m.print(*args())
     d = os.path.join(findproject(), infodirname)
     target = os.path.join(d, thisbranch())
     os.makedirs(os.path.dirname(target), exist_ok = True)
