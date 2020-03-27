c4d12ecb9cd5fd17c0b4b60d2d5259df54bf370e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-03-27 10:57:48 +0000

    oh theres a generous limit

--- dev_bin/k8slogs.py
+++ dev_bin/k8slogs.py
@@ -6,6 +6,7 @@ from lagoon import date
 import logging, sys
 
 log = logging.getLogger(__name__)
+maxsize = 10000
 
 def main_k8slogs():
     logging.basicConfig(format = "[%(levelname)s] %(message)s", level = logging.INFO)
@@ -21,9 +22,12 @@ def main_k8slogs():
     except NoSuchPathException:
         pass
     es = Elasticsearch(context.resolved('elasticsearch', 'hosts').unravel())
-    result = es.search(allow_partial_search_results = False, body = dict(query = dict(bool = dict(must = [
+    result = es.search(size = maxsize, allow_partial_search_results = False, body = dict(query = dict(bool = dict(must = [
         dict(match = {'kubernetes.container_name': config.container_name}), # TODO: Match whole field not substring.
         dict(range = {'@timestamp': dict(gte = date._Iseconds._d(f"{config.ago} ago").rstrip())}),
     ]))))
-    for source in (hit['_source'] for hit in result['hits']['hits']):
+    hits = result['hits']['hits']
+    for source in (hit['_source'] for hit in hits):
         getattr(sys, source['stream']).write(source['message'])
+    if len(hits) == maxsize:
+        log.warning('Data likely truncated.')
