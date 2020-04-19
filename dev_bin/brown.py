e52204ff07c9df97a09048b8c938c692e5341379
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-04-19 17:29:34 +0100

    allow targeted browning

--- dev_bin/brown.py
+++ dev_bin/brown.py
@@ -1,12 +1,17 @@
 from .common import findproject
 from lagoon import autopep8, sed
-import subprocess, re
+import re, subprocess, sys
 
 cols = 120
 
 def main_brown():
     'Satisfy PEP 8 with minimal impact.'
-    command = autopep8._rv.partial('--max-line-length', cols, findproject())
+    args = sys.argv[1:]
+    if args:
+        root, = args
+    else:
+        root = findproject()
+    command = autopep8._rv.partial('--max-line-length', cols, root)
     result = command._d(stdout = subprocess.DEVNULL, stderr = subprocess.PIPE)
     def paths():
         for line in result.splitlines():
