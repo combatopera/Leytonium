ca5abc3cbd7895acf88a2748d89d9bbe0d82801b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-26 13:23:11 +0100

    fix

--- dev_bin/next.py
+++ dev_bin/next.py
@@ -1,20 +1,19 @@
-from .common import findproject
 from lagoon import git
-import os
+from pathlib import Path
 
-def main_next(): # FIXME: Does not work in submodule.
+def main_next():
     'Go to next step in current git workflow.'
-    gitdir = os.path.join(findproject(), '.git')
-    if os.path.isdir(os.path.join(gitdir, 'rebase-apply')):
+    gitdir = Path(git.rev_parse.__git_dir().rstrip())
+    if (gitdir / 'rebase-apply').is_dir():
         if git.status.__porcelain().splitlines():
             git.rebase.__continue.print()
         else:
             git.rebase.__skip.print()
-    elif os.path.isfile(os.path.join(gitdir, 'MERGE_HEAD')):
+    elif (gitdir / 'MERGE_HEAD').is_file():
         git.commit.__no_edit.print()
-    elif os.path.isfile(os.path.join(gitdir, 'CHERRY_PICK_HEAD')):
+    elif (gitdir / 'CHERRY_PICK_HEAD').is_file():
         git.cherry_pick.__continue.print()
-    elif os.path.isfile(os.path.join(gitdir, 'REVERT_HEAD')):
+    elif (gitdir / 'REVERT_HEAD').is_file():
         git.revert.__continue.print()
     else:
         raise Exception('Unknown git workflow, giving up.')
