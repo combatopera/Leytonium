0ab15e18b0d29ff039e81ba19685b2cce7640d3c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-10-21 14:25:33 +0100

    comment

--- dev_bin/tempvenv.py
+++ dev_bin/tempvenv.py
@@ -10,6 +10,6 @@ def main_tempvenv():
     'Activate a temporary venv.'
     with TemporaryDirectory() as venvdir:
         venvdir = Path(venvdir)
-        python3._m.venv.print(venvdir)
+        python3._m.venv.print(venvdir) # Must use host executable to get pip apparently.
         Program.text(venvdir / 'bin' / 'pip').install.print(*sys.argv[1:])
         Program.text(shellpath)._c.print('. "$1" && "$2"', '-c', venvdir / 'bin' / 'activate', shellpath)
