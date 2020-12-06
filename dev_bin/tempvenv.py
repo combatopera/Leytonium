651f657d3f54b42ba73f64d9330162da3f09404e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-06 19:25:07 +0000

    packages are installed differently depending on whether wheel is available

--- dev_bin/tempvenv.py
+++ dev_bin/tempvenv.py
@@ -1,17 +1,27 @@
+from argparse import ArgumentParser
 from lagoon import python3
 from lagoon.program import Program
 from pathlib import Path
 from tempfile import TemporaryDirectory
-import os, sys
+import logging, os
 
+log = logging.getLogger(__name__)
 shellpath = os.environ['SHELL']
 
 def main_tempvenv():
     'Activate a temporary venv.'
+    logging.basicConfig(level = logging.DEBUG, format = "[%(levelname)s] %(message)s")
+    parser = ArgumentParser()
+    parser.add_argument('-w', action = 'store_true', help = 'enable bdist_wheel command')
+    parser.add_argument('reqs', nargs = '*')
+    args = parser.parse_args()
     with TemporaryDirectory() as venvdir:
         venvdir = Path(venvdir)
+        log.info("Create venv: %s", venvdir)
         python3._m.venv.print(venvdir) # Must use host executable to get pip apparently.
-        reqs = sys.argv[1:]
-        if reqs:
-            Program.text(venvdir / 'bin' / 'pip').install.print(*reqs)
+        pipinstall = Program.text(venvdir / 'bin' / 'pip').install
+        if args.w:
+            pipinstall.wheel.print()
+        if args.reqs:
+            pipinstall.print(*args.reqs)
         Program.text(shellpath)._c.print('. "$1" && exec "$2"', '-c', venvdir / 'bin' / 'activate', shellpath)
