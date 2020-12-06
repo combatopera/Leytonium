3b7a87a29ff278d656e734f758be3f4a7d40c01b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-06 23:35:58 +0000

    that should be legal now

--- leytonium/diffuse.py
+++ leytonium/diffuse.py
@@ -1,3 +1,23 @@
+# Copyright 2020 Andrzej Cichocki
+
+# This file is part of Leytonium.
+#
+# Leytonium is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Leytonium is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
+
+# This file incorporates work covered by the following copyright and
+# permission notice:
+
 # Copyright (C) 2006-2019 Derrick Moser <derrick_moser@yahoo.com>
 # Copyright (C) 2015-2020 Romain Failliot <romain.failliot@foolstep.com>
 #
