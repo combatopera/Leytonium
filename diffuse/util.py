308cf7e640169abfe5fe9fa354549fa39399e707
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-12 19:34:22 +0000

    no

--- diffuse/util.py
+++ diffuse/util.py
@@ -45,7 +45,7 @@ APP_NAME = 'Diffuse'
 # use the program's location as a starting place to search for supporting files
 # such as icon and help documentation
 bin_dir = os.path.dirname(sys.executable if hasattr(sys, 'frozen') else os.path.realpath(sys.argv[0]))
-COPYRIGHT =  '''{copyright} \N{COPYRIGHT SIGN} 2006-2019 Derrick Moser
+COPYRIGHT = '''{copyright} \N{COPYRIGHT SIGN} 2006-2019 Derrick Moser
 {copyright} \N{COPYRIGHT SIGN} 2015-2020 Romain Failliot'''.format(copyright=_("Copyright"))
 # platform test
 isWindows = os.name == 'nt'
