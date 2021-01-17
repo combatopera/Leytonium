27fb5982fcef22173fcfc04c3132dbc5c14cefd2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-01-17 22:28:43 +0000

    option to first remove all but current kernel

--- leytonium/upgrade.py
+++ leytonium/upgrade.py
@@ -15,12 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
 
+from argparse import ArgumentParser
 from lagoon import sudo
 from pathlib import Path
 
 def main_upgrade():
     'Upgrade the system and silence the nag.'
+    parser = ArgumentParser()
+    parser.add_argument('-k', action = 'store_true', help = 'first remove all backup kernels')
+    args = parser.parse_args()
     apt_get = sudo.apt_get[print]
+    if args.k:
+        current = Path('/boot/vmlinuz').resolve()
+        apt_get.remove(*(p.name.replace('vmlinuz', 'linux-image') for p in Path('/boot').glob('vmlinuz-*-generic') if p != current))
     apt_get.update()
     apt_get.__with_new_pkgs.upgrade()
     apt_get.autoremove()
