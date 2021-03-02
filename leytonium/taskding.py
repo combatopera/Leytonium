82b962acac6aa5c7df232330e10acb1d00a6c765
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-03-02 19:37:42 +0000

    make sound path configurable

--- leytonium/taskding.py
+++ leytonium/taskding.py
@@ -15,38 +15,47 @@
 # You should have received a copy of the GNU General Public License
 # along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
 
+from aridity.config import ConfigCtrl
+from diapyr.util import innerclass
 from lagoon import pgrep
 from lagoon.program import bg
 from pathlib import Path
 import os, subprocess, sys, time
 
 sleeptime = .5
-soundpath = Path('/usr/share/sounds/freedesktop/stereo/complete.oga')
 threshold = 5
 alwaysinteractive = {'Concern', 'diffuse', 'gt', 'man', 'showstash', 'top', 'vim'}
 
-class Child:
+class ChildFactory:
 
-    def __init__(self, start):
-        self.start = start
+    def __init__(self, config):
+        self.sound_path = Path(config.sound.path)
 
-    def fetch(self, pid):
-        try:
-            with open(f"/proc/{pid}/comm") as f:
-                self.armed = f.read().rstrip() not in alwaysinteractive
-                return True
-        except (FileNotFoundError, ProcessLookupError):
-            pass
+    @innerclass
+    class Child:
 
-    def fire(self, now):
-        from lagoon import paplay
-        if self.start + threshold <= now and self.armed and soundpath.exists() and not os.fork():
-            paplay[exec](soundpath)
+        def __init__(self, start):
+            self.start = start
+
+        def fetch(self, pid):
+            try:
+                with open(f"/proc/{pid}/comm") as f:
+                    self.armed = f.read().rstrip() not in alwaysinteractive
+                    return True
+            except (FileNotFoundError, ProcessLookupError):
+                pass
+
+        def fire(self, now):
+            from lagoon import paplay
+            if self.start + threshold <= now and self.armed and self.sound_path.exists() and not os.fork():
+                paplay[exec](self.sound_path)
 
 def main_taskding():
     'Play a sound when a long-running child of shell terminates.'
+    config = ConfigCtrl().loadappconfig(main_taskding, 'taskding.arid')
     if 'SSH_CLIENT' in os.environ:
         return
+    factory = ChildFactory(config)
     shpidstr, = sys.argv[1:]
     children = {}
     while True:
@@ -55,7 +64,7 @@ def main_taskding():
         try:
             with pgrep[bg]('-P', shpidstr) as stdout:
                 for line in stdout:
-                    nowchildren[int(line)] = Child(now)
+                    nowchildren[int(line)] = factory.Child(now)
         except subprocess.CalledProcessError:
             break
         for pid in children.keys() - nowchildren.keys():
