e599ba5e97faaee8aff1334cd6f097cfca89f4a9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-11-19 13:59:53 +0000

    configg

--- leytonium/spamtrash.py
+++ leytonium/spamtrash.py
@@ -28,15 +28,13 @@ def main_spamtrash():
     cc = ConfigCtrl()
     cc.node.keyring = keyring
     config = cc.loadappconfig(main_spamtrash, 'spamtrash.arid')
-    mail = IMAP4_SSL(config.host)
+    imap = IMAP4_SSL(config.host)
     with config.password as password:
-        mail.login(config.user, password)
-    #_, v = mail.list()
-    #for x in v: print(x)
-    mail.select('[Gmail]/Spam')
-    _, (v,) = mail.search(None, 'ALL')
+        imap.login(config.user, password)
+    imap.select(config.mailbox)
+    _, (v,) = imap.search(None, 'ALL')
     ids = number.findall(v)
     for id in ids:
-        _, data = mail.fetch(id, '(RFC822)')
+        _, data = imap.fetch(id, '(RFC822)')
         print(message_from_string(data[0][1].decode('latin-1')))
         break
