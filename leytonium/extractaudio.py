88f87c60e959d581a26ed97c20dcde792346c1d8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2022-08-30 19:56:38 +0100

    add option to delete input files

--- leytonium/extractaudio.py
+++ leytonium/extractaudio.py
@@ -1,4 +1,5 @@
 from . import initlogging
+from argparse import ArgumentParser
 from lagoon import ffmpeg
 from lagoon.program import partial
 from pathlib import Path
@@ -8,7 +9,14 @@ log = logging.getLogger(__name__)
 
 def main_extractaudio():
     initlogging()
-    for inpath in map(Path, sys.argv[1:]):
+    parser = ArgumentParser()
+    parser.add_argument('-d', action = 'store_true')
+    parser.add_argument('path', nargs = '+', type = Path)
+    args = parser.parse_args()
+    for inpath in args.path:
         outpath = inpath.with_suffix('.aac')
         log.info("Extract: %s", outpath)
         ffmpeg._i[partial](inpath)._vn._acodec.copy(outpath)
+        if args.d:
+            log.info("Delete: %s", inpath)
+            inpath.unlink()
