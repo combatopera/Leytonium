f64b47323c4708586c63799116ae4e856dc1fb32
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-09-26 14:59:22 +0100

    indicate child count of hidden item

--- leytonium/t.py
+++ leytonium/t.py
@@ -19,22 +19,44 @@
 from lagoon import tree
 from lagoon.program import partial
 from lagoon.util import stripansi
+from pathlib import Path
 import re, sys
 
-denymatch = re.compile('── [.]').search
+intro = '── '
+denymatch = re.compile(f"{re.escape(intro)}[.]").search
+indent = 4
 
 def main():
     allow = True
     allowmatch = None
+    parts = []
     with tree._aC[partial](*sys.argv[1:]) as f:
         for line in f:
             bwline = stripansi(line)
             if not allow and allowmatch(bwline) is not None:
                 allow = True
             if allow:
-                sys.stdout.write(line)
+                try:
+                    depth, r = divmod(bwline.index(intro) + len(intro), indent)
+                    assert not r
+                except ValueError:
+                    depth = 0
+                name, = bwline[depth * indent:].splitlines()
+                del parts[depth:]
+                parts.append(name)
+                path = Path(*parts)
                 m = denymatch(bwline)
-                if m is not None:
+                if m is None:
+                    sys.stdout.write(line)
+                else:
+                    sanseol, = line.splitlines()
+                    sys.stdout.write(sanseol)
+                    try:
+                        n = sum(1 for _ in path.iterdir())
+                    except (FileNotFoundError, NotADirectoryError):
+                        n = 0
+                    sys.stdout.write('/' * n)
+                    sys.stdout.write(line[len(sanseol):])
                     allow = False
                     allowmatch = re.compile(f".{{,{m.start()}}}── ").match
 
