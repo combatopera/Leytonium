033763f415659519fa7043e743f52c654ad5d8c6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-06 10:28:05 +0000

    mount source read only

--- leytonium/vcp.py
+++ leytonium/vcp.py
@@ -34,12 +34,12 @@ def main():
     parser.parse_args(namespace = config.cli)
     target = config.target
     log.info("Create: %s", target)
-    docker.volume.create[print](target)
+    docker.volume.create[print](target) # XXX: Can we make this fail if the volume already exists?
     mount_source = config.mount.source
     mount_target = config.mount.target
     source = config.source
     log.info('Copy data.')
-    docker.run.__rm[print]('-v', f"{source}:{mount_source}", '-v', f"{target}:{mount_target}", '-w', mount_source, config.image, 'sh', '-c', f"exec cp -av . {shlex.quote(mount_target)}")
+    docker.run.__rm[print]('-v', f"{source}:{mount_source}:ro", '-v', f"{target}:{mount_target}", '-w', mount_source, config.image, 'sh', '-c', f"exec cp -av . {shlex.quote(mount_target)}")
     if config.delete:
         log.info("Delete: %s", source)
         docker.volume.rm[print](source)
