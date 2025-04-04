ded71631081d4ade39d78bc85ed2887e7c9f39fd
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-04-04 21:23:18 +0100

    detach venv so motivate does not behave weird

--- leytonium/terminator.py
+++ leytonium/terminator.py
@@ -16,9 +16,44 @@
 # along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
 
 'Unset SHLVL before launching Terminator in case Cinnamon was wrapped by shell.'
-import os, sys
+from . import initlogging
+from lagoon.text import lsof
+from pathlib import Path
+import logging, os, re, sys
+
+log = logging.getLogger(__name__)
+
+class Venv:
+
+    @classmethod
+    def detachall(cls):
+        sepregex = re.escape(os.sep)
+        pattern = re.compile(f"n(.+){sepregex}readlocks{sepregex}.+")
+        v = lsof._F.fn('-p', os.getpid()).splitlines()
+        for f, n in zip(v[1::2], v[2::2]):
+            m = pattern.fullmatch(n)
+            if m is not None:
+                cls(m.group(1), int(f[1:])).detach()
+
+    def __init__(self, path, fd):
+        self.bindir = Path(path, 'bin')
+        self.fd = fd
+
+    def _bindirs(self):
+        for p in os.environ['PATH'].split(os.pathsep):
+            if Path(p) == self.bindir:
+                log.debug("Filter out: %s", p)
+            else:
+                yield p
+
+    def detach(self):
+        os.environ['PATH'] = os.pathsep.join(self._bindirs())
+        log.debug("Close: %s", self.fd)
+        os.close(self.fd)
 
 def main():
+    initlogging()
+    Venv.detachall()
     os.environ.pop('SHLVL')
     os.execv('/usr/bin/terminator', sys.argv)
 
