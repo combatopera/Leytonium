9986937bd251744a8fa45ea038bb297a225fdc05
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-05-28 20:56:55 +0100

    retire formatting feature

--- leytonium/gt.py
+++ leytonium/gt.py
@@ -16,22 +16,14 @@
 # along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
 
 'Stage all outgoing changes and show them.'
-from . import brown, st
+from . import st
 from .common import findproject
-from aridity.config import ConfigCtrl
-from foyndation import dotpy
 from lagoon.text import git
 from pathlib import Path
 
 def main():
-    config = (-ConfigCtrl().loadappconfig((brown.__name__, 'brown'), 'common.arid')).reapplysettings(main)
     projectdir = Path(findproject()).resolve()
-    paths = [projectdir / line[line.index("'") + 1:-1] for line in git.add._n(projectdir).splitlines()]
-    if projectdir.name in config.formattedprojects:
-        toformat = [path for path in paths if path.exists() and path.name.endswith(dotpy)]
-        if toformat:
-            brown.brown(config.cols, toformat)
-    git.add[print](*paths)
+    git.add[print](*(projectdir / line[line.index("'") + 1:-1] for line in git.add._n(projectdir).splitlines()))
     st.main()
 
 if '__main__' == __name__:
