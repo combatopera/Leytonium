fa14fed517aec9980391ec5d56d3b17aba11757b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-09-21 14:36:41 +0100

    looks to be unused

--- leytonium/stmulti.py
+++ leytonium/stmulti.py
@@ -49,7 +49,6 @@ class Project:
         for command in self.commands:
             setattr(self, Path((-command).path).name, (-command).cd(path))
         self.homerelpath = path.resolve().relative_to(effectivehome)
-        self.netpath = Path(config.repomount, effectivehome.name, self.homerelpath)
         self.shortnetpath = Path(config.shortrepomount, effectivehome.name, self.homerelpath)
         self.config = config
         self.path = path
