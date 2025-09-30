3c241612b8282f18b71b4998f94287cd6cc2f0f1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-09-30 21:13:31 +0100

    refactoring

--- leytonium/mortcalc.py
+++ leytonium/mortcalc.py
@@ -18,9 +18,14 @@
 'Instant mortgage statement.'
 from aridity.config import ConfigCtrl
 from datetime import date, timedelta
+from functools import cache
 
 maxmonth = timedelta(31)
 oneday = timedelta(1)
+
+@cache
+def _yearlen(year):
+    return int((date(year + 1, 1, 1) - date(year, 1, 1)) / oneday)
 
 def main():
     config = ConfigCtrl().loadappconfig(main, 'mortcalc.arid')
@@ -32,23 +37,22 @@ def main():
     value = config.value
     holidays = set(map(date.fromisoformat, config.holiday))
     while mark <= lastmark:
-        dayrate = rate / int((date(mark.year + 1, 1, 1) - date(mark.year, 1, 1)) / oneday)
+        nextmark = (mark + maxmonth).replace(day = 1)
+        dayrate = rate / _yearlen(mark.year)
         paymark = mark
         while paymark.weekday() > 4 or paymark in holidays:
             paymark += oneday
-        nextmark = (mark + maxmonth).replace(day = 1)
-        days1 = int((paymark - mark) / oneday)
-        days2 = int((nextmark - paymark) / oneday)
+        payments = {paymark: payment, nextmark: 0}
         interest = 0
-        for d in range(days1):
-            interest += balance * dayrate
-            b = balance + interest
-            print(mark + timedelta(d), interest, b, b / value * 100)
-        balance -= payment
-        for d in range(days2):
-            interest += balance * dayrate
-            b = balance + interest
-            print(mark + timedelta(days1 + d), interest, b, b / value * 100)
+        cursor = mark
+        for m, p in sorted(payments.items()):
+            days = int((m - cursor) / oneday)
+            for d in range(days):
+                interest += balance * dayrate
+                b = balance + interest
+                print(cursor + timedelta(d), interest, b, b / value * 100)
+            cursor = m
+            balance -= p
         balance += round(interest, 2)
         mark = nextmark
 
