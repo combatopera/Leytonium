a678eea0da1b5c0ea9a76bbd0b0e60be2045f835
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2025-12-08 10:52:43 +0000

    another 'common' message

--- leytonium/git_completion.bash
+++ leytonium/git_completion.bash
@@ -25,7 +25,7 @@ function _demonames {
 }
 
 function _commonmessages {
-    COMPREPLY=($(compgen -W 'conciser refactor refactoring undup redundant mv ren inline formatting unused idiomaticer consistency obsolete comment documentation efficienter experimentally convention simplify retire sensibler extract invalid generaler logging correcter quieter specificer reliabler eleganter temporarily minimaler determinism tastefuler preciser expliciter' $2))
+    COMPREPLY=($(compgen -W 'conciser refactor refactoring undup redundant mv ren inline formatting unused idiomaticer consistency obsolete comment documentation efficienter experimentally convention simplify retire sensibler extract invalid generaler logging correcter quieter specificer reliabler eleganter temporarily minimaler determinism tastefuler preciser expliciter realisticer' $2))
 }
 
 function _unconflictedpaths {
